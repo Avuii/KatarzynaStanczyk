33753210366b3b389ac9a77d1e6037f2251857ac
Avui <k.stanczyk4364@gmail.com>
2026-02-06 18:40:41 +0100

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+   base: "/KatarzynaStanczyk/",
   plugins: [
     react(),
     tailwindcss(),
-    base: "/KatarzynaStanczyk/",
   ],
   resolve: {
     alias: {
